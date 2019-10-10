6586ddcf3a00865a03ee8ee23217ef4484b56ed3
残梦 <2662419405@qq.com>
2019-10-10 16:32:06 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {Button} from 'antd-mobile'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        123
+        <Button type="primary">开始</Button>
+      </div>
+    )
+  }
+}
