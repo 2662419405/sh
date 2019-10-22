837703987df64b21c48ede651fd7e94a9126c235
残梦 <2662419405@qq.com>
2019-10-22 14:00:18 +0800

    '测试域名'

--- src/redux/chat-redux.js
+++ src/redux/chat-redux.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 import io from 'socket.io-client'
-const socket = io('ws://localhost:9093')
+const socket = io('ws://shtodream.cn:9093')
 
 //获取聊天列表 
 const MSG_LIST = 'MSG_LIST'
